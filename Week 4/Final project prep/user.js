5bf3da8f23d786524a1b4a02c6ab4893b451b7fe
Barsatzulkarnine <mohammedbarsatzulkarnine@gmail.com>
2023-01-18 12:31:58 +0600

    completed

--- Week 4/Final project prep/user.js
+++ Week 4/Final project prep/user.js
@@ -1,21 +1,23 @@
+const postListEl = document.querySelector(".post-list");
+const id = localStorage.getItem("id");
 
-// API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
-
-const postListEl =  document.querySelector('.post-list')
-
-async function main (){
-   const id =  localStorage.getItem("id");
-   const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
-   const postsData = await posts.json();
-   console.log(postsData)
-
-    postListEl.innerHTML =  postsData.map( post => userHtml(post)).join("")
+async function onSearchChange(event) {
+  //   console.log(event.target.value)
+  const id = event.target.value;
+  renderPost(id);
 }
 
-main();
+async function renderPost(id) {
+  const posts = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?userId=${id}`
+  );
+  const postsData = await posts.json();
+  console.log(postsData);
+  postListEl.innerHTML = postsData.map((post) => postHtml(post)).join("");
+}
 
-function userHtml (user) {
-    return `
+function postHtml(user) {
+  return `
     <div class="post">
        <div class="post__title">
          ${user.title}
@@ -24,6 +26,7 @@ function userHtml (user) {
          ${user.body}
        </p>
      </div>
-     `
+     `;
 }
 
+renderPost(id);
