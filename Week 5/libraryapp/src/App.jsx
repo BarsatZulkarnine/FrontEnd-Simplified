10aacd046b45bce16445c68477e90099125ab8a1
Barsatzulkarnine <mohammedbarsatzulkarnine@gmail.com>
2023-02-06 12:18:03 +0600

    working on checkkout

--- Week 5/libraryapp/src/App.jsx
+++ Week 5/libraryapp/src/App.jsx
@@ -1,16 +1,25 @@
-import './App.css';
-import Highlight from './components/Highlights';
-import Landing from './components/Landing page';
-import Nav from "./components/nav"
+import "./App.css";
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Books from "./Pages/Books";
+import { books } from "./data";
+import BookInfo from "./Pages/BookInfo";
+import Cart from "./Pages/Cart";
 
 function App() {
   return (
-    <div className="App">
-      <Nav/>
-      <Landing/>
-      <Highlight/>
-
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/books" exact element={<Books books={books} />} />
+          <Route path="/books/:id" element={<BookInfo books={books} />} />
+          <Route path="/cart" element = {<Cart books={books} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
